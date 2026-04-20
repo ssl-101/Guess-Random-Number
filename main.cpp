cab059aaa2c3655943273b6bec8bf61e430db7ac
ssl-101 <Inquiresiham@gmail.com>
2026-04-20 18:41:53 +0000

    added variable for the program to read

--- main.cpp
+++ main.cpp
@@ -5,16 +5,30 @@ using namespace std;
 
 int main()
 {
-
-  string file;
+ string file;
  ifstream inFile;
+
+   inFile.open(file);
 
   cout<< "Enter the file name:";
   cin >> file;
 
   if (!inFile) {
     cout<< "Error with opening file!"<<endl;
-    return(1)
+    return 1;
+  
+    // starting counter at 0
+  string student, first, last;
+    int count 0;
+
+    if (inFile >> student)
+
+    {
+      count++
+        if(student < first);
+        if(student > last );
+    }
+    
     
 
     
