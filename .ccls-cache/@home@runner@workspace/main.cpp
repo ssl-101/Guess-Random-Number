539108b7926779f5b87c2628b0b2b93b36c8e3bc
ssl-101 <Inquiresiham@gmail.com>
2026-04-21 02:41:39 +0000

    created random generator (1,25)

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -4,6 +4,14 @@
 using namespace std;
 int main()
 {
- 
+  int randomNumber, inputNumber, guessTotal= 0;
+
+  //random generator
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_int_distribution<> dist (1,25)
+
+  randomNumber= dist(gen)
+    
   return 0; 
 }
