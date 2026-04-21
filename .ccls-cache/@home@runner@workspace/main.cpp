a803b8ccdac21f1f6532d13dcb080b207d20dcea
ssl-101 <Inquiresiham@gmail.com>
2026-04-21 02:29:02 +0000

    set up random library

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,23 +1,9 @@
-//This program will order students alphabetically.
+//this program will display patterns
 #include <iostream>
-#include <fstream>
+#include <random>
 using namespace std;
-
 int main()
 {
-
-  string file;
- ifstream file;
-
-  cout<< "Enter the file name:";
-  cin >> file;
-
-  if (!file.open()) {
-    cout<< "Error with opening file!"<<endl;
-
-    
-  }
-
-  
-  
+ 
+  return 0; 
 }
--- main.cpp
+++ main.cpp
@@ -1,27 +1,9 @@
 //this program will display patterns
 #include <iostream>
+#include <random>
 using namespace std;
 int main()
 {
-  //A
-
-  for (int i = 1; i <= 10 ; i++)
-    {
-     for (int j= 0; j < i; j++)
-      {
-        cout<<"*";
-      }
-        cout<<endl;
-    }
-  // B
-
-  for (int i =10; i >=1; i--)
-    {
-      for(int j =0; j< i; j++)
-        {
-       cout<<"*";
-    }
-       cout << endl;
-    }
+ 
   return 0; 
 }
